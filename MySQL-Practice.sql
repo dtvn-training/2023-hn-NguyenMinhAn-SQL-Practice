fc459c0c549ecd89392a406e8c2f214ac6333520
KawaiiHasuu <annm01032002@gmail.com>
2023-11-14 16:54:08 +0700

    Test advanced sql

--- MySQL-Practice.sql
+++ MySQL-Practice.sql
@@ -1,14 +1,16 @@
 
+use pos_system;
+
 # 1. DCL PRACTICE
 
-# select user();
-# show grants for root;
+select user();
+show grants for root;
 
-# create user 'test'@'localhost' identified by "pass123"
-# select user, host from mysql.user;
+create user 'test'@'localhost' identified by "pass123";
+select user, host from mysql.user;
 
-# grant all privileges on *.* to 'test'@'localhost';
-# show grants for test@localhost;
+grant all privileges on *.* to 'test'@'localhost';
+show grants for test@localhost;
 
 
 
@@ -19,73 +21,73 @@
 
 # 2. DDL PRACTICE     
 
-# create database pos_system;
-# create table products (
-# 	id int(11) not null,
-#     name varchar(255),
-# 	description varchar(255) not null,
-#     price double not null
-# );
+create database pos_system;
+create table products (
+	id int(11) not null,
+    name varchar(255),
+	description varchar(255) not null,
+    price double not null
+);
 
-# describe products;
+describe products;
 
-# drop table products;
+drop table products;
 
-# create table products (
-# 	id int not null,
-#     name varchar(255) not null,
-#     description varchar(255) not null,
-#     primary key (id)
-# );
+create table products (
+	id int not null,
+    name varchar(255) not null,
+    description varchar(255) not null,
+    primary key (id)
+);
 
-# drop table products;
+drop table products;
 
-# create table products (
-# 	id int not null,
-#     name varchar(255), 
-#     code varchar(20),
-#     description varchar(255) not null,
-#     unique (code),
-#     primary key (id)
-# );
+create table products (
+	id int not null,
+    name varchar(255), 
+    code varchar(20),
+    description varchar(255) not null,
+    unique (code),
+    primary key (id)
+);
 
-# drop table product_categories;
-# create table product_categories (
-# 	id int primary key,
-#     name varchar(255) 
-# );
+drop table products;
+create table product_categories (
+	id int primary key,
+    name varchar(255) 
+);
 
-# create table products (
-# 	id int(11) not null primary key,
-#     name varchar(255) not null, 
-#     product_category_id int(11), 
-#     constraint FK_category foreign key (product_category_id) references product_categories(id)
-# );
+create table products (
+	id int(11) not null primary key,
+    name varchar(255) not null, 
+    product_category_id int(11), 
+    constraint FK_category foreign key (product_category_id) references product_categories(id)
+);
 
 # TEST VIEW
-# create view ProductsView as
-# select name, product_category_id 
-# from products;
+create view ProductsView as
+select name, product_category_id 
+from products;
 
-# select * from ProductsView;
-# drop view ProductsView;
+select * from ProductsView;
+drop view ProductsView;
 
-# alter database pos_system character set = ascii;
+alter database pos_system character set = ascii;
 
-# alter table products 
-# add (
-# 	price double,
-#     description varchar(255)
-# );
+alter table products 
+add (
+	price double,
+    description varchar(255)
+);
 
-# alter table products 
-# modify description varchar(1000);
+alter table products 
+modify description varchar(1000);
 
-# alter table products 
-# add index index_p_category(product_category_id);
+alter table products 
+add index index_p_category(product_category_id);
 
-# alter table product_categories 
-# rename to product_categories_list;
+alter table product_categories 
+rename to product_categories_list;
 
 
 
@@ -97,49 +99,66 @@
 
 # 3. DML
 
-# insert into products values (8, "Rice", 3, 60000, "Rice");
-# insert into product_categories_list values (4, "Other");
+insert into products values 
+(1, "Rice", 3, 40000, "Rice"),
+(2, "Fried Rice", 3, 50000, "Fried Rice"),
+(3, "Tea", 3, 20000, "Tea"),
+(4, "Milk Tea", 3, 20000, "Milk Tea"),
+(5, "Red bull", 3, 23000, "Red bull"),
+(6, "Nutri", 3, 24000, "Nutri"),
+(7, "Lavie", 3, 5000, "Lavie"),
+(8, "C2", 3, 9000, "C2"),
+(9, "Egg", 3, 1000, "Egg"),
+(10, "C3", 3, 9000, "C2"),
+(11, "C4", 3, 9000, "C2");
 
-# select * from products, product_categories_list;
+insert into product_categories values 
+(1, "Drink"),
+(2, "Food"),
+(3, "Other");
 
-# update products set price = 35000 where id = 6;
+drop table product_categories_list;
 
-# select * from products;
+select * from products, product_categories_list;
 
-# select distinct product_category_id
-# from products;
+update products set price = 35000 where id = 6;
 
-# select * from products 
-# order by price desc, id asc;
+select * from products;
 
-# select * from products limit 2;
+select distinct product_category_id
+from products;
 
-# select name from products
-# where price in (20000, 30000);
+select * from products 
+order by price desc, id asc;
 
-# select count(*) as count_coffee from products 
-# where name like "%ff%";
+select * from products limit 2;
 
-# select product_category_id, sum(price) as total, count(product_category_id) as quantity
-# from products
-# group by product_category_id
-# having sum(price) < 100000
-# order by sum(price) asc;
+select name from products
+where price in (20000, 30000);
 
-# select name, price from products 
-# where product_category_id in 
-# (select id from product_categories_list where name in ("Coffee", "Drink"));
+select count(*) as count_coffee from products 
+where name like "%ff%";
+
+select product_category_id, sum(price) as total, count(product_category_id) as quantity
+from products
+group by product_category_id
+having sum(price) < 100000
+order by sum(price) asc;
+
+select name, price from products 
+where product_category_id in 
+(select id from product_categories_list where name in ("Coffee", "Drink"));
 
 
-# select product_category_id from products union select id from product_categories_list;
+select product_category_id from products union select id from product_categories_list;
 
-# select products.name, product_categories_list.name 
-# from products inner join product_categories_list 
-# on products.product_category_id = product_categories_list.id;
+select products.name, product_categories_list.name 
+from products inner join product_categories_list 
+on products.product_category_id = product_categories_list.id;
 
-# select products.name, product_categories_list.name 
-# from products right join product_categories_list 
-# on products.product_category_id = product_categories_list.id;
+select products.name, product_categories_list.name 
+from products right join product_categories_list 
+on products.product_category_id = product_categories_list.id;
 
 
 
@@ -149,20 +168,60 @@
 
 # 4. TCL
 
-# start transaction;
-# delete from products where id = 3;
-# savepoint sp1;
-# delete from products where id = 2;
-# rollback to savepoint sp1;
-# commit;
+start transaction;
+delete from products where id = 3;
+savepoint sp1;
+delete from products where id = 2;
+rollback to savepoint sp1;
+commit;
 
-# start transaction;
-# savepoint sp;
-# drop table products;
-# rollback to savepoint sp;
-# commit;
+start transaction;
+savepoint sp;
+drop table products;
+rollback to savepoint sp;
+commit;
 
-# select * from products;
+select * from products;
+
+
+
+
+
+
+
+
+
+
+# Advanced
+
+explain select * from products where name like "%av%";
+
+explain analyze select * from products where name like "%ed%";
+
+# Test Index Selectivity
+
+select count(distinct product_category_id)/count(*) as sel1,
+count(distinct left(name, 2)) / count(*) as sel2,
+count(distinct left(name, 3)) / count(*) as sel3,
+count(distinct left(name, 4)) / count(*) as sel4
+from products;
+
+# Test Composite Index
+
+create index name_price on products (`name`, `price`);
+
+explain select * from products where name = 'C2';
+explain select * from products where price = 9000;
+explain select * from products where name = 'C2' and price = 9000;
+
+# Test Index Merge
+
+create index name_idx on products (`name`);
+create index price_idx on products (`price`);
+explain select * from products where name = 'C2' or price = 9000;
+
+
+
 
 
 
